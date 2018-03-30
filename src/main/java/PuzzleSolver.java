7ed2df6adccbc126df3f0db8ee7708fbe70580de
Tufman, Shay (st198j) <st198j@intl.att.com>
2018-03-30 17:46:18 +0300

    Add Skeleton for Solver

--- src/main/java/PuzzleSolver.java
+++ src/main/java/PuzzleSolver.java
@@ -18,6 +18,27 @@ public class PuzzleSolver {
         printSolverInputList();
         printSolverAvailableNumOfSolution();
         printOptionsForResults();
+
+        PuzzleElement startWithTopLeftCorner = null;
+        try{
+            startWithTopLeftCorner = jigsawElementInputList.get(availableOptionsForSolution.get(PUZZLEDIRECTIONS.TOP_LEFT_CORNER).get(0));
+        }
+        catch (IndexOutOfBoundsException e){
+            //TODO - Finish the game....
+        }
+
+        findSolutionFor(startWithTopLeftCorner);
+    }
+
+    private void findSolutionFor(PuzzleElement startWithTopLeftCorner) {
+        System.out.println("Recursive Function - As appear below:");
+        System.out.println("Since TopLeftCorner always should exist -> In case not, no available solution!!!");
+        System.out.println("Upon given the NumOfElements, and num of rows ==>> num of columns can be calculated");
+        System.out.println("First we will seek for the 1st row, so 1st row is full");
+        System.out.println("In case we won't have it, no available solution");
+        System.out.println("Than, for each element for the 1st row, we will try to fill its column");
+        System.out.println("keep in mind to \"save\" LEFT_0 for the 1st column, and RIGHT_0 for last column");
+        System.out.println("1st Row, with TOP_0, and last row BOTTOM_0");
     }
 
     private void printOptionsForResults() {
