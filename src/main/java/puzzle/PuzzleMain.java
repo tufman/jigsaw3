8f93df98ef9e640f6f9e427b478d5260cffd6651
Yelena Koviar <yk4184@att.com>
2018-05-03 23:48:16 +0300

    fix code according to code review

--- src/main/java/puzzle/PuzzleMain.java
+++ src/main/java/puzzle/PuzzleMain.java
@@ -5,66 +5,12 @@ import java.time.LocalDateTime;
 
 public class PuzzleMain {
 
-    private static boolean isRotation;
-    private static boolean isMultiThread;
-    private static String filePath;
-    private static String filePathToSave;
-    private static int numOfThreads;
-
     public static void main(String[] args) throws IOException, InterruptedException {
-        extractParameters(args);
-        LocalDateTime timeStart = LocalDateTime.now();
         PuzzleManager puzzleManager = new PuzzleManager();
-        puzzleManager.manage(filePath, filePathToSave, isRotation, isMultiThread, numOfThreads);
+        LocalDateTime timeStart = LocalDateTime.now();
+        puzzleManager.extractParameters(args);
+        puzzleManager.manage();
         LocalDateTime timeEnd = LocalDateTime.now();
         System.out.println("run time : " + (timeEnd.toString() + " " + timeStart.toString()));
-    }
-
-    private static void extractParameters(String[] args) {
-        if (args.length == 0)
-        {
-            printUsgae();
-        }
-        int paramLocation =0;
-
-        for (String arg : args){
-            paramLocation++;
-            if (arg.equals("-input")){
-                filePath = args[paramLocation];
-                continue;
-            }
-            if (arg.equals("-output")){
-                filePathToSave = args[paramLocation];
-                continue;
-            }
-            if (arg.equals("-rotate")){
-                isRotation = true;
-                continue;
-            }
-            if (arg.equals("-threads")){
-                try{
-                    numOfThreads = Integer.parseInt(args[paramLocation]);
-                }
-                catch(NumberFormatException | ArrayIndexOutOfBoundsException e){
-                    numOfThreads = 4;
-                    isMultiThread = true;
-                }
-                continue;
-            }
-        }
-
-        if ((filePath == null) || (filePathToSave == null)){
-            printUsgae();
-        }
-    }
-
-    private static void printUsgae() {
-        System.out.println("There were no commandline arguments passed!");
-
-        System.out.println("The below parameters should appaer");
-        System.out.println("-input <path> - full path for the Puzzle input file");
-        System.out.println("-output <fileout> - full path for the Puzzle output file");
-        System.out.println("-rotate - in case support rotation is required, in case not appear - will be set to false;");
-        System.out.println("-thread <numOfThreads> - in case flag not appear, the default will be 1. In case wrong number, the default is 4");
     }
 }
--- src/main/java/puzzle/PuzzleManager.java
+++ src/main/java/puzzle/PuzzleManager.java
@@ -4,25 +4,75 @@ import java.io.IOException;
 
 public class PuzzleManager {
 
-private PuzzleElement[][] board = null;
+    private PuzzleElement[][] board = null;
+    private boolean isRotation;
+    private boolean isMultiThread;
+    private int numOfThreads;
+    private static String filePath;
+    private static String filePathToSave;
 
+    public void manage() throws IOException {
 
-public void manage(String filePath, String filePathToOutput, boolean isRotation, boolean isMultiThread, int numOfThreads) throws IOException {
-
-    Puzzle puzzle1 = new Puzzle();
-
-
-    WritePuzzleStatus writePuzzleStatus = new WritePuzzleStatus(filePathToOutput);
-       if(puzzle1.readInputFile(filePath, isRotation, isMultiThread, numOfThreads)) {
-           PuzzleSolver puzzleSolver = new PuzzleSolver(puzzle1);
-           board = puzzleSolver.solve();
-           //write puzzle solution to output file
-           writePuzzleStatus.WriteResultToFile(board);
-           //sout
-           puzzle1.printSolution();
-
-       }else {
-           writePuzzleStatus.WriteErrorsToFile(puzzle1.getErrorsReadingInputFile());
-       }
+        Puzzle puzzle1 = new Puzzle();
+        WritePuzzleStatus writePuzzleStatus = new WritePuzzleStatus(filePathToSave);
+        if (puzzle1.readInputFile(filePath, isRotation, isMultiThread, numOfThreads)){
+            PuzzleSolver puzzleSolver = new PuzzleSolver(puzzle1);
+            board = puzzleSolver.solve();
+            //write puzzle solution to output file
+            writePuzzleStatus.WriteResultToFile(board);
+            //sout
+            puzzle1.printSolution();
+        } else{
+            writePuzzleStatus.WriteErrorsToFile(puzzle1.getErrorsReadingInputFile());
+        }
     }
+
+    public void extractParameters(String[] args) {
+        if (args.length == 0) {
+            printUsgae();
+        }
+        int paramLocation = 0;
+
+        for (String arg : args) {
+            paramLocation++;
+            if (arg.equals("-input")) {
+                filePath = args[paramLocation];
+                continue;
+            }
+            if (arg.equals("-output")) {
+                filePathToSave = args[paramLocation];
+                continue;
+            }
+            if (arg.equals("-rotate")) {
+                isRotation = true;
+                continue;
+            }
+            if (arg.equals("-threads")) {
+                try {
+                    numOfThreads = Integer.parseInt(args[paramLocation]);
+                } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+                    numOfThreads = 4;
+                    isMultiThread = true;
+                }
+                continue;
+            }
+        }
+
+        if ((filePath == null) || (filePathToSave == null)) {
+            printUsgae();
+        }
+    }
+
+    private static void printUsgae() {
+        System.out.println("There were no commandline arguments passed!");
+
+        System.out.println("The below parameters should appaer");
+        System.out.println("-input <path> - full path for the Puzzle input file");
+        System.out.println("-output <fileout> - full path for the Puzzle output file");
+        System.out.println("-rotate - in case support rotation is required, in case not appear - will be set to false;");
+        System.out.println("-thread <numOfThreads> - in case flag not appear, the default will be 1. In case wrong number, the default is 4");
+    }
+
+
 }
+
