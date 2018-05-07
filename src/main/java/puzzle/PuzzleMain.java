a9d7fdd98899c14b962ce7e701e1313b20009b14
Tufman, Shay (st198j) <st198j@intl.att.com>
2018-05-07 15:29:25 +0300

    Remove un-used code from solver

--- src/main/java/puzzle/PuzzleMain.java
+++ src/main/java/puzzle/PuzzleMain.java
@@ -8,10 +8,15 @@ public class PuzzleMain {
 
     public static void main(String[] args) throws IOException, InterruptedException, ExecutionException {
         PuzzleManager puzzleManager = new PuzzleManager();
-        LocalDateTime timeStart = LocalDateTime.now();
+        //LocalDateTime timeStart = LocalDateTime.now();
+        //long startTime = System.nanoTime();
+        long startTime = System.currentTimeMillis();
         puzzleManager.extractParameters(args);
         puzzleManager.manage();
-        LocalDateTime timeEnd = LocalDateTime.now();
-        System.out.println("run time : " + (timeEnd.toString() + " " + timeStart.toString()));
+        //LocalDateTime timeEnd = LocalDateTime.now();
+        long endTime   = System.currentTimeMillis();
+        long totalTime = endTime - startTime;
+        //System.out.println("run time : " + (timeEnd.toString() + " " + timeStart.toString()));
+        System.out.println("Total time (Milliseconds) " + totalTime);
     }
 }
--- src/main/java/puzzle/PuzzleSolver.java
+++ src/main/java/puzzle/PuzzleSolver.java
@@ -17,7 +17,6 @@ import java.util.concurrent.atomic.AtomicBoolean;
 public class PuzzleSolver {
     private int numOfThreads;
     private List<PuzzleElement> elements = new ArrayList<>();
-    private ArrayList<Integer> rowOptions;
     private Map<Integer, List<PuzzleElement>> puzzleStructure;
     private List<Integer> usedElementById;
     private PuzzleElement[][] board;
@@ -76,32 +75,6 @@ public class PuzzleSolver {
         return solutionBoard;
     }
 
-//        for (int i=0; i<numOfThreads ; i++) {
-//
-//
-//            if (resultFound.get()){
-//
-////                Future<String> future1 = executorService.submit(stringCallable);
-////                System.out.println(future1.get());
-//                //executorService.shutdown();
-//                return solutionBoard;
-//            }else{
-//                System.out.println(Thread.currentThread().getName() +  "Atomic boolean is still false");
-//                executorService.shutdown();
-//            }
-//
-//        }
-//
-//
-//
-//        executorService.shutdown();
-//        return null;
-//        }
-
-
-
-//    }
-
     private void initPuzzle(int columns, int rows) {
         this.columns = columns;
         this.rows = rows;
@@ -115,9 +88,10 @@ public class PuzzleSolver {
      * @return
      */
     private PuzzleElement[][] solve(int r, int c) {
-       // if (isSolved(r,c) && (!(resultFound.get())))
-        if (isSolved(r,c))
+        if (isSolved(r,c)){
             return board;
+        }
+
         int key= createKey(r,c);
         List<PuzzleElement> puzzleElements = puzzleStructure.get(key);
         if(puzzleElements==null){
@@ -125,24 +99,21 @@ public class PuzzleSolver {
         }
         // Try each remaining piece in this square
         for (PuzzleElement p : puzzleElements) {
-            if(inUse(p))
+            if(inUse(p)){
                 continue;
-            if (tryInsert(p, r, c)) {
+            }
+
+                board[r][c]=p;
                 setAsUsed(p);
-                // Find the next position
                 Position next = nextPos(r, c);
-                // Recurse to try next square
                 PuzzleElement[][] solution = solve(next.row, next.column);
-                if (solution != null) {
+                if (solution != null && (!(resultFound.get())) ) {
                     System.out.println("Solution was found by " + Thread.currentThread().getName());
                     resultFound.set(true);
-                // This sequence worked - success!
                     solutionBoard = solution;
                     return solution;
                 }
-            }else {
-                continue;
-            }
+
             setAsNotUsed(p);
         }
         // no solution with this piece
@@ -181,113 +152,6 @@ public class PuzzleSolver {
         return  left * 1000 + top * 100 + right*10 + bottom;
     }
 
-    /**
-     * put piece in to board
-     * @param e
-     * @param r
-     * @param c
-     * @return
-     */
-    private boolean tryInsert(PuzzleElement e, int r, int c) {
-
-        // TOP_LEFT_CORNER for one row solution
-        if (r == 0 && c == 0 && r == rows -1 ) {
-            if (!fit(e, getPuzzleList(getKey(0, 0, 5, 0)))) return false;
-        }
-        //  TOP_LEFT_CORNER for one column solution
-        else if (c == 0 && r == 0 && c==columns-1) {
-            if ( !(fit(e, getPuzzleList(getKey(0,0,0,5)))))
-                return false;
-        }
-        //TOP_LEFT_CORNER 7
-        if (r == 0 && c == 0) {
-            if (!fit(e, getPuzzleList(getKey(0, 0, 0, 7)))) return false;
-        }
-        // BOTTOM_LEFT_CORNER 777
-        else if (r == rows-1 && c == 0) {
-            if (!(fit(e, getPuzzleList(getKey(0,7,7,7)))&&(board[r-1][c].getBottom()+e.getTop() == 0))) return false;
-        }
-        // TOP_RIGHT_CORNER 77
-        else if (r == 0 && c==columns-1) {
-            if ( !(fit(e, getPuzzleList(getKey(0,0,7,7)))&& (board[r][c-1].getRight()+e.getLeft() == 0))) return false;
-        }
-        // BOTTOM_RIGHT_CORNER 7777
-        else if (r == rows -1 && c==columns-1) {
-            if (!(fit(e, getPuzzleList(getKey(7,7,7,7)))&& (board[r][c-1].getRight()+e.getLeft() == 0)
-                    && (board[r-1][c].getBottom()+e.getTop() == 0))) return false;
-        }
-        // one row solution
-        else if (r == rows -1 && r == 0) {
-            if ( !(fit(e, getPuzzleList(getKey(e.getLeft(),0,5,0))) && (board[r][c-1].getRight()+e.getLeft() == 0)))
-                return false;
-        }
-        //check if edge
-        else if (r == 0) { // first row
-            if ( !(fit(e, getPuzzleList(getKey(e.getLeft(),0,5,5))) && (board[r][c-1].getRight()+e.getLeft() == 0)))
-                return false;
-        }
-        // last row
-        else if (r == rows -1) {
-            if ( !(fit(e, getPuzzleList(getKey(e.getLeft(),e.getTop(),5,0))) && (board[r-1][c].getBottom()+e.getTop() == 0)
-                    && (board[r][c-1].getRight()+e.getLeft() == 0)))
-                return false;
-        }
-        //  one column solution
-        else if (c == 0 && c==columns-1) {
-            if (!(fit(e, getPuzzleList(getKey(0,e.getTop(),0,5))) && (board[r-1][c].getBottom()+e.getTop() == 0)))
-                return false;
-        }
-        // first column
-        else if (c == 0) {
-            if ( !(fit(e, getPuzzleList(getKey(0,e.getTop(),5,5))) && (board[r-1][c].getBottom()+e.getTop() == 0)))
-                return false;
-        }
-        // last column
-        else if (c==columns-1) {
-            if ( !(fit(e, getPuzzleList(getKey(e.getLeft(),e.getTop(),0,5))) && (board[r-1][c].getBottom()+e.getTop() == 0)
-                    && (board[r][c-1].getRight()+e.getLeft() == 0)))
-                return false;
-        }
-        // middle element
-        else {
-            if ( !((board[r-1][c].getBottom()+e.getTop() == 0)&& (board[r][c-1].getRight()+e.getLeft() == 0)))
-                return false;
-        }
-        board[r][c]=e;
-        return true;
-    }
-
-    /**
-     * generate key
-     * @param l
-     * @param t
-     * @param r
-     * @param b
-     * @return
-     */
-    private Integer getKey(int l, int t, int r, int b) {
-        return l*1000 + t*100 + r*10 + b;
-    }
-
-    /**
-     * check if element exist on right plaice in Map
-     * @param pe
-     * @param puzzleList
-     * @return
-     */
-    private boolean fit(PuzzleElement pe, List<PuzzleElement> puzzleList) {
-        if(puzzleList==null) return false;
-        return puzzleList.contains(pe);
-    }
-
-    /**
-     * get List of puzzle elements from map by using key
-     * @param key
-     * @return
-     */
-    public List<PuzzleElement> getPuzzleList(Integer key) {
-        return puzzleStructure.get(key);
-    }
 
     /**
      * set element as used to avoid reuse
