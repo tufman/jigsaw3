d980a38bb0a92c414c1222865a961fa74afbb5f8
Tufman, Shay (st198j) <st198j@intl.att.com>
2018-05-18 23:46:00 +0300

    Basic Logging & 1Hr connection open

--- src/main/java/puzzle/PuzzleServerMain.java
+++ src/main/java/puzzle/PuzzleServerMain.java
@@ -1,11 +1,27 @@
 package puzzle;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.IOException;
 import java.util.concurrent.ExecutionException;
 
 public class PuzzleServerMain {
 
+    private static Logger logger = LogManager.getLogger(PuzzleServerMain.class);
+
     public static void main(String[] args) throws IOException, InterruptedException, ExecutionException {
+
+        logger.info("INFO - Main From Puzzle Server - Before Loop");
+        logger.error("ERROR - Main From Puzzle Server - Before Loop");
+        logger.fatal("FATAL - Main From Puzzle Server - Before Loop");
+        for (int i =0; i < 1000; i++){
+            logger.info("INFO - Main From Puzzle Server");
+            logger.error("ERROR - Main From Puzzle Server");
+            logger.fatal("FATAL - Main From Puzzle Server");
+        }
+
+
         PuzzleManager puzzleManager = new PuzzleManager();
         long startTime = System.currentTimeMillis();
         puzzleManager.extractParameters(args);
